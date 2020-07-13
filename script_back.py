1a08caaf17a39733f9908f38e754df80579ab271
memolinda <linda.giacomozzi@gmail.com>
2020-07-13 11:34:43 +0200

    backend: implemented the insert and the view function

--- script_back.py
+++ script_back.py
@@ -6,3 +6,31 @@ def connect():
     cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
     conn.commit()
     conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ? , ?, ?, ?)", (title, author,year,isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""): #when the user search only one parameters the function assign an empty value for the others
+    conn = sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * From book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+connect()
+# insert('The EARTH', 'John Tablet', 1918, 3984375838)
+print(view())
+print(search(author='John Tablet'))
