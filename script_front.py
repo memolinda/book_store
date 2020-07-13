44a29b87d862e9e14ff9e24e993bfe94e79ccff9
memolinda <linda.giacomozzi@gmail.com>
2020-07-13 14:43:35 +0200

    frontend: shows the entries of the click entry into the corresponding box, connect the update button to the update function, close button destroy the window

--- script_front.py
+++ script_front.py
@@ -34,9 +34,20 @@ def get_selected_row(event):
     global selected_tuple    #to be able to call the local variable in the global program
     index=list1.curselection()[0]
     selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END, selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END, selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END, selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END, selected_tuple[4])
 
 def delete_command():
     script_back.delete(selected_tuple[0])
+
+def update_command():
+    script_back.update(selected_tuple[0], e1_value.get(), e2_value.get(), e3_value.get(), e4_value.get())
 
 window = Tk()
 window.title("Book storage")
@@ -89,13 +100,13 @@ b2.grid(row=3, column=3)
 b3=Button(window, text="Add entry", width=12, command=insert_command)
 b3.grid(row=4, column=3)
 
-b4=Button(window, text="Update selected", width=12)
+b4=Button(window, text="Update selected", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
 b5=Button(window, text="Delete selected", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6=Button(window, text="Close", width=12)
+b6=Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.mainloop()
